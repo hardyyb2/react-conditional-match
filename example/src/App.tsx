989494f5725b51c6717596b22fcc0c636f498fb0
Hardik_B <hardik.badola@yellow.ai>
2023-02-26 16:08:53 +0530

    [ADDED] conditions in example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,36 @@
+import { ConditionalMatch } from "@dx-kit/react-conditional-match";
 import { useState } from "react";
 import "./App.css";
-import { ConditionalMatch } from "@dx-kit/react-conditional-match";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
+
   return (
-    <ConditionalMatch fallback={<div>fallback</div>}>
-      <ConditionalMatch.Render when={false}>
-        <div>Child 1</div>
-      </ConditionalMatch.Render>
-      <ConditionalMatch.Render when={false}>
-        <div>Child 2</div>
-      </ConditionalMatch.Render>
-      <div>Will not render</div>
-    </ConditionalMatch>
+    <div className='w-full / grid place-items-center gap-y-8 / text-2xl'>
+      <div className='flex gap-4'>
+        <button className='btn btn-secondary' onClick={handleDecrement}>
+          - decrement
+        </button>
+        <button className='btn btn-primary' onClick={handleIncrement}>
+          + increment
+        </button>
+      </div>
+      <ConditionalMatch fallback={<div>{count} is greater than 10</div>}>
+        <ConditionalMatch.Render when={count < 5}>{count} is less than 5</ConditionalMatch.Render>
+        <ConditionalMatch.Render when={count === 5}>{count} is equal to 5</ConditionalMatch.Render>
+        <ConditionalMatch.Render when={count > 5 && count < 10}>{count} is equal to 5</ConditionalMatch.Render>
+
+        <div>Will not render since its not a `Render` component</div>
+      </ConditionalMatch>
+    </div>
   );
 }
 
